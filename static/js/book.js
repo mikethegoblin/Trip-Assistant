9b59b68758f5af98657ea3eef92e51ba6116b8c2
Yansong Bai <yansong@YansongdeMacBook-Pro.local>
2022-11-26 15:38:38 -0500

    fix duration

--- static/js/book.js
+++ static/js/book.js
@@ -1,54 +1,85 @@
-document.addEventListener('DOMContentLoaded', () => {
-    flight_duration();
+document.addEventListener("DOMContentLoaded", () => {
+  flight_duration();
 });
 
-function flight_duration() {
-    document.querySelectorAll(".duration").forEach(element => {
-        let time = element.dataset.value.split(":");
-        element.innerText = time[0]+"h "+time[1]+"m";
-    });
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
 }
 
+function convertMsToHM(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  seconds = seconds % 60;
+  // 👇️ if seconds are greater than 30, round minutes up (optional)
+  minutes = seconds >= 30 ? minutes + 1 : minutes;
+
+  minutes = minutes % 60;
+
+  // 👇️ If you don't want to roll hours over, e.g. 24 to 00
+  // 👇️ comment (or remove) the line below
+  // commenting next line gets you `24:00:00` instead of `00:00:00`
+  // or `36:15:31` instead of `12:15:31`, etc.
+  hours = hours % 24;
+
+  return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}`;
+}
+
+function flight_duration() {
+  document.querySelectorAll(".duration").forEach((element) => {
+    let time = convertMsToHM(element.dataset.value).split(":");
+    element.innerText = time[0] + "h " + time[1] + "m";
+  });
+}
 
 function add_traveller() {
-    let div = document.querySelector('.add-traveller-div');
-    let fname = div.querySelector('#fname');
-    let lname = div.querySelector('#lname');
-    let gender = div.querySelectorAll('.gender');
-    let gender_val = null
-    if(fname.value.trim().length === 0) {
-        alert("Please enter First Name.");
-        return false;
-    }
+  let div = document.querySelector(".add-traveller-div");
+  let fname = div.querySelector("#fname");
+  let lname = div.querySelector("#lname");
+  let gender = div.querySelectorAll(".gender");
+  let gender_val = null;
+  if (fname.value.trim().length === 0) {
+    alert("Please enter First Name.");
+    return false;
+  }
 
-    if(lname.value.trim().length === 0) {
-        alert("Please enter Last Name.");
-        return false;
-    }
+  if (lname.value.trim().length === 0) {
+    alert("Please enter Last Name.");
+    return false;
+  }
 
-    if (!gender[0].checked) {
-        if (!gender[1].checked) {
-            alert("Please select gender.");
-            return false;
-        }
-        else {
-            gender_val = gender[1].value;
-        }
+  if (!gender[0].checked) {
+    if (!gender[1].checked) {
+      alert("Please select gender.");
+      return false;
+    } else {
+      gender_val = gender[1].value;
     }
-    else {
-        gender_val = gender[0].value;
-    }
+  } else {
+    gender_val = gender[0].value;
+  }
 
-    let passengerCount = div.parentElement.querySelectorAll(".each-traveller-div .each-traveller").length;
+  let passengerCount = div.parentElement.querySelectorAll(
+    ".each-traveller-div .each-traveller"
+  ).length;
 
-    let traveller = `<div class="row each-traveller">
+  let traveller = `<div class="row each-traveller">
                         <div>
-                            <span class="traveller-name">${fname.value} ${lname.value}</span><span>,</span>
+                            <span class="traveller-name">${fname.value} ${
+    lname.value
+  }</span><span>,</span>
                             <span class="traveller-gender">${gender_val.toUpperCase()}</span>
                         </div>
-                        <input type="hidden" name="passenger${passengerCount+1}FName" value="${fname.value}">
-                        <input type="hidden" name="passenger${passengerCount+1}LName" value="${lname.value}">
-                        <input type="hidden" name="passenger${passengerCount+1}Gender" value="${gender_val}">
+                        <input type="hidden" name="passenger${
+                          passengerCount + 1
+                        }FName" value="${fname.value}">
+                        <input type="hidden" name="passenger${
+                          passengerCount + 1
+                        }LName" value="${lname.value}">
+                        <input type="hidden" name="passenger${
+                          passengerCount + 1
+                        }Gender" value="${gender_val}">
                         <div class="delete-traveller">
                             <button class="btn" type="button" onclick="del_traveller(this)">
                                 <svg width="1.1em" height="1.1em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -58,51 +89,55 @@ function add_traveller() {
                             </button>
                         </div>
                     </div>`;
-    div.parentElement.querySelector(".each-traveller-div").innerHTML += traveller;
-    div.parentElement.querySelector("#p-count").value = passengerCount+1;
-    div.parentElement.querySelector(".traveller-head h6 span").innerText = passengerCount+1;
-    div.parentElement.querySelector(".no-traveller").style.display = 'none';
-    fname.value = "";
-    lname.value = "";
-    gender.forEach(radio => {
-        radio.checked = false;
-    });
+  div.parentElement.querySelector(".each-traveller-div").innerHTML += traveller;
+  div.parentElement.querySelector("#p-count").value = passengerCount + 1;
+  div.parentElement.querySelector(".traveller-head h6 span").innerText =
+    passengerCount + 1;
+  div.parentElement.querySelector(".no-traveller").style.display = "none";
+  fname.value = "";
+  lname.value = "";
+  gender.forEach((radio) => {
+    radio.checked = false;
+  });
 
-    let pcount = document.querySelector("#p-count").value;
-    let fare = document.querySelector("#basefare").value;
-    let fee = document.querySelector("#fee").value;
-    if (parseInt(pcount)!==0) {
-        document.querySelector(".base-fare-value span").innerText = parseInt(fare)*parseInt(pcount);
-        document.querySelector(".total-fare-value span").innerText = (parseInt(fare)*parseInt(pcount))+parseInt(fee);
-    }
-
+  let pcount = document.querySelector("#p-count").value;
+  let fare = document.querySelector("#basefare").value;
+  let fee = document.querySelector("#fee").value;
+  if (parseInt(pcount) !== 0) {
+    document.querySelector(".base-fare-value span").innerText =
+      parseInt(fare) * parseInt(pcount);
+    document.querySelector(".total-fare-value span").innerText =
+      parseInt(fare) * parseInt(pcount) + parseInt(fee);
+  }
 }
 
 function del_traveller(btn) {
-    let traveller = btn.parentElement.parentElement;
-    let tvl = btn.parentElement.parentElement.parentElement.parentElement;
-    let cnt = tvl.querySelector("#p-count");
-    cnt.value = parseInt(cnt.value)-1;
-    tvl.querySelector(".traveller-head h6 span").innerText = cnt.value;
-    if(parseInt(cnt.value) <= 0) {
-        tvl.querySelector('.no-traveller').style.display = 'block';
-    }
-    traveller.remove();
-    
-    let pcount = document.querySelector("#p-count").value;
-    let fare = document.querySelector("#basefare").value;
-    let fee = document.querySelector("#fee").value;
-    if (parseInt(pcount) !== 0) {
-        document.querySelector(".base-fare-value span").innerText = parseInt(fare)*parseInt(pcount);
-        document.querySelector(".total-fare-value span").innerText = (parseInt(fare)*parseInt(pcount))+parseInt(fee);   
-    }
+  let traveller = btn.parentElement.parentElement;
+  let tvl = btn.parentElement.parentElement.parentElement.parentElement;
+  let cnt = tvl.querySelector("#p-count");
+  cnt.value = parseInt(cnt.value) - 1;
+  tvl.querySelector(".traveller-head h6 span").innerText = cnt.value;
+  if (parseInt(cnt.value) <= 0) {
+    tvl.querySelector(".no-traveller").style.display = "block";
+  }
+  traveller.remove();
+
+  let pcount = document.querySelector("#p-count").value;
+  let fare = document.querySelector("#basefare").value;
+  let fee = document.querySelector("#fee").value;
+  if (parseInt(pcount) !== 0) {
+    document.querySelector(".base-fare-value span").innerText =
+      parseInt(fare) * parseInt(pcount);
+    document.querySelector(".total-fare-value span").innerText =
+      parseInt(fare) * parseInt(pcount) + parseInt(fee);
+  }
 }
 
 function book_submit() {
-    let pcount = document.querySelector("#p-count");
-    if(parseInt(pcount.value) > 0) {
-        return true;
-    }
-    alert("Please add atleast one passenger.")
-    return false;
+  let pcount = document.querySelector("#p-count");
+  if (parseInt(pcount.value) > 0) {
+    return true;
+  }
+  alert("Please add atleast one passenger.");
+  return false;
 }
