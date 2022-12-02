21da6a4f46c36ada3517624db34fcdbbcb3e0c96
Zeyu Gu <mikegu0508@gmail.com>
2022-12-02 15:38:03 -0500

    update database schema

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
     username = db.Column(db.String(255), unique=True, nullable=False)
-    # email = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=True)
     password = db.Column(db.Text, nullable=False)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
